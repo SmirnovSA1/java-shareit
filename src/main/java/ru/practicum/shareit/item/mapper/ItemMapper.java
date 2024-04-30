cc0817328e211151868bfd69dbc828395a727a61
SmirnovSA <smirnov.dak0ta@yandex.ru>
2024-04-30 17:05:17 +0300

    refactor: добавить библиотеку mapStruct и настроить для совместно работы с lombok. Также изменить класс ItemMapper на интерфейс с добалением используемых методов, чтобы библиотека могла сгенерировать в target класс-реализацию.

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,26 +1,13 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
-public class ItemMapper {
-    public static ItemDto toItemDto(Item item) {
-        return ItemDto.builder()
-                .id(item.getId())
-                .name(item.getName())
-                .description(item.getDescription())
-                .available(item.getAvailable())
-                .request(item.getRequest() != null ? item.getRequest().getId() : null)
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface ItemMapper {
 
-    public static Item toItem(Long userId, ItemDto item) {
-        return Item.builder()
-                .id(item.getId())
-                .name(item.getName())
-                .description(item.getDescription())
-                .available(item.getAvailable())
-                .owner(userId)
-                .request(null).build();
-    }
+    ItemDto toItemDto(Item item);
+
+    Item toItem(Long userId, ItemDto item);
 }
